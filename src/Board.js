71f8fab37c8f87bbde0a345bde284169f89986a6
AJ Magracia <ajmagracia@gmail.com>
2018-09-19 23:15:29 -0700

    Changed a lot of "board" to "grid", removed logo

--- src/Board.js
+++ src/Board.js
@@ -6,108 +6,105 @@ class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
-      board: []
+      grid: []
     }
-  }
-
-  componentDidMount() {
-    if (this.state.board.length >= 1) return false
-    this.createBoard( {
+    this._createGrid( {
       rowLength: 15,
       columnLength: 15
     } )
   }
 
   render() {
-    let { board } = this.state
-    let gameBoard = board.map( ( row, rowIndex ) => {
+    let { grid } = this.state
+    let board = grid.map( ( row, rowIndex ) => {
       return row.map( ( column, columnIndex ) => {
         return <Square
         key={rowIndex + ', ' + columnIndex}
         row={rowIndex}
         column={columnIndex}
-        bomb={board[rowIndex][columnIndex]}/>
+        bomb={grid[rowIndex][columnIndex]}/>
       } )
     } )
     return (
-      <div className="game-board">{gameBoard}</div>
+      <div className="game-board">{board}</div>
     )
   }
 
-
-
-  // create a board of dimensions rowLength x columnLength with bombs
-  createBoard = ( { rowLength, columnLength } ) => {
+  // create a grid of dimensions rowLength x columnLength with bombs
+  _createGrid = ( { rowLength, columnLength } ) => {
     let squares = rowLength * columnLength
-    let numbers = this.createNumberArray(squares)
+    let numbers = this.createArray(squares)
     let bombNumbers = this.createBombNumbers(squares,numbers)
 
-    // turn the bomb numbers into coordinates on board
+    // turn the bomb numbers into coordinates on grid
     let bombCoordinates = bombNumbers.map( number => {
-      console.log(number)
       let row = Math.floor( number / rowLength )
-
       let column = number % columnLength
       return [ row, column ]
     } )
 
-    let board = []
     /*
     For loop to push a row of columns for rowLength amount of times.
-    Cannot use a nested map because of react mapping rules...til I figure it out
+    Cannot use a map because of react mapping rules...til I figure it out
     */
-    for ( let i = 0; i < rowLength; i++ ) {
-      board.push( this.createArray( columnLength, 0 ) )
-    }
+    // let grid = []
+    // for ( let i = 0; i < rowLength; i++ ) {
+    //   grid.push( this.createArray( columnLength, 0 ) )
+    // }
+
+    // Figured it out... Is mapping even better? The code is bulkier...
+    let row = this.createArray( rowLength, 0)
+    let columnSlots = this.createArray( columnLength, 0)
+    // Spread operator to *duplicate* code
+    let grid = columnSlots.map( slot => ( [ ...row ] ) )
 
     // places a bomb in the appropriate squares
     bombCoordinates.forEach( coordinate => {
-      console.log(coordinate)
-      board[ coordinate[ 0 ] ][ coordinate[ 1 ] ] = "B"
+      grid[ coordinate[ 0 ] ][ coordinate[ 1 ] ] = "B"
     } )
 
-
-    // place numbers denoting adjacent bombs in appropriate squares
+    // Loop to place numbers denoting adjacent bombs in appropriate squares
     // i is the index of the row, and there are columnLength amount of rows
     for ( let i = 0; i < columnLength; i++ ) {
       //j is the index of the column, and there are rowLength amount of columns
       for ( let j = 0; j < rowLength; j++ ) {
         // do not do anything unless we've found a bomb
-        if ( board[ i ][ j ] !== "B" ) continue
-        //
+        if ( grid[ i ][ j ] !== "B" ) continue
+        /*
+        start at the square 1 unit up and to the left and go top to bottom,
+        left to right
+        */
         for ( let v = -1; v <= 1; v++ ) {
           for ( let h = -1; h <= 1; h++ ) {
             let [ y, x ] = [ v + i, h + j ]
-            if ( typeof board[ y ] !== 'undefined' ) {
-              if ( typeof board[ y ][ x ] === 'number' )
-                board[ y ][ x ] += 1
+            if ( typeof grid[ y ] !== 'undefined' ) {
+              if ( typeof grid[ y ][ x ] === 'number' )
+                grid[ y ][ x ] += 1
             }
           }
         }
       }
     }
-
-    this.setState( {
-      board
-    } )
+    this.state.grid = grid
   }
   /*
   create an array of numbers from 1 to rowLength * columnLength
-  each digit represents a square on the board
+  each digit represents a square on the grid
   */
-  createNumberArray = (number) => {
-    let numbers = []
-    // for loop to create an array of sequential integers with length = number
-    for ( let i = 0; i <= number; i++ ) {
-      numbers.push( i )
-    }
-    return numbers
-  }
+  // createNumberArray = (number) => {
+  //   let numbers = []
+  //   // for loop to create an array of sequential integers with length = number
+  //   for ( let i = 0; i <= number; i++ ) {
+  //     numbers.push( i )
+  //   }
+  //   return numbers
+  // }
 
   createArray = (size, item) => {
     let array = []
     for (let i = 0; i<size; i++) {
-      array.push(item)
+      let thing = item === undefined ? i : item
+      array.push(thing)
     }
     return array
   }
