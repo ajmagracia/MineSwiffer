04920a33fe8762aaaad0ca67dbf745ea3faa715c
AJ Magracia <ajmagracia@gmail.com>
2018-09-19 19:34:41 -0700

    refactored creating number array, bomb numbers, bomb placement

--- src/Board.js
+++ src/Board.js
@@ -34,60 +34,13 @@ class Board extends Component {
     )
   }
 
-  increaseBombCounter = ({horizontalDirection, verticalDirection}) => {
-    // dd
-  }
 
-  createArray = (size, item) => {
-    let array = []
-    for (let i = 0; i<size; i++) {
-      array.push(item)
-    }
-    return array
-  }
 
   // create a board of dimensions rowLength x columnLength with bombs
-  createBoard = ( {
-    rowLength,
-    columnLength
-  } ) => {
-    let board = []
-    /*
-    for loop to push a row of columns for rowLength amount of times
-    cannot use a nested map because of react mapping rules
-    */
-    for ( let i = 0; i < rowLength; i++ ) {
-      board.push( this.createArray( columnLength, 0 ) )
-    }
-
-    /*
-    create an array of numbers from 1 to rowLength * columnLength
-    each digit represents a square on the board
-    */
+  createBoard = ( { rowLength, columnLength } ) => {
     let squares = rowLength * columnLength
-    let numbers = []
-    // for loop to create an array with length = number of squares
-    for ( let i = 0; i <= squares; i++ ) {
-      numbers.push( i )
-    }
-
-
-    let bombNumbers = []
-    let bombAmount = Math.floor( Math.sqrt( squares+1 ) )
-    // For loop to put out 10 unique numbers (square identifiers)
-    for ( let i = 1; i <= bombAmount; ++i ) {
-      // pick a random integer between 1 and the number of squares
-      let n = Math.floor( ( Math.random() * ( squares - i ) ) )
-      // put the integer into the bombNumbers array
-      bombNumbers.push( numbers[ n ] )
-      // replace the number we pushed with the last number
-      numbers[ n ] = numbers[ squares - i ]
-      /*
-      if the last number is the number we picked, nothing happens
-      we do this because after every iteration,
-      we decrease the upper limit of n by 1
-      */
-    }
+    let numbers = this.createNumberArray(squares)
+    let bombNumbers = this.createBombNumbers(squares,numbers)
 
     // turn the bomb numbers into coordinates on board
     let bombCoordinates = bombNumbers.map( number => {
@@ -98,6 +51,15 @@ class Board extends Component {
       return [ row, column ]
     } )
 
+    let board = []
+    /*
+    For loop to push a row of columns for rowLength amount of times.
+    Cannot use a nested map because of react mapping rules...til I figure it out
+    */
+    for ( let i = 0; i < rowLength; i++ ) {
+      board.push( this.createArray( columnLength, 0 ) )
+    }
+
     // places a bomb in the appropriate squares
     bombCoordinates.forEach( coordinate => {
       console.log(coordinate)
@@ -105,67 +67,74 @@ class Board extends Component {
     } )
 
 
-
     // place numbers denoting adjacent bombs in appropriate squares
-    for (let i = 0; i < rowLength; i++) {
-      let [up, down] = [i-1, i+1]
-      for (let j = 0; j < columnLength; j++) {
-        // first find a bomb
-        if (board[i][j] !== "B") continue
-        let [left, right] = [j-1, j+1]
-        // for row above
-        if (i!==0) {
-          if (j!==0) {
-            // if (!board[up][left]) {
-              if (typeof board[i][left] !== 'string')
-                board[up][left] += 1
-            }
-
-          if (typeof board[up][j] !== 'string') {
-            board[up][j] += 1
-          }
-          if (j!==14 & typeof board[up][right] !== 'string') {
-            board[up][right] += 1
-          }
-        }
-        // for same row
-        if (j!==0) {
-          if (typeof board[i][left] !== 'string') {
-            board[i][left] += 1
-          }
-        }
-        if (j!==14){
-          if (typeof board[i][right] !== 'string') {
-            board[i][right] += 1
-          }
-        }
-        // for row below
-        if (i!==14) {
-          if (j!==0) {
-            if (!board[down][left]) {
-              if (!board[down][left])
-                board[down][left] += 1
+    // i is the index of the row, and there are columnLength amount of rows
+    for ( let i = 0; i < columnLength; i++ ) {
+      //j is the index of the column, and there are rowLength amount of columns
+      for ( let j = 0; j < rowLength; j++ ) {
+        // do not do anything unless we've found a bomb
+        if ( board[ i ][ j ] !== "B" ) continue
+        //
+        for ( let v = -1; v <= 1; v++ ) {
+          for ( let h = -1; h <= 1; h++ ) {
+            let [ y, x ] = [ v + i, h + j ]
+            if ( typeof board[ y ] !== 'undefined' ) {
+              if ( typeof board[ y ][ x ] === 'number' )
+                board[ y ][ x ] += 1
             }
           }
-          if (typeof board[down][j] !== 'string') {
-            board[down][j] += 1
-          }
-          if (j!==14 & typeof board[down][right] !== 'string') {
-            board[down][right] += 1
-          }
         }
-
-
-
       }
-
     }
 
     this.setState( {
       board
     } )
   }
+  /*
+  create an array of numbers from 1 to rowLength * columnLength
+  each digit represents a square on the board
+  */
+  createNumberArray = (number) => {
+    let numbers = []
+    // for loop to create an array of sequential integers with length = number
+    for ( let i = 0; i <= number; i++ ) {
+      numbers.push( i )
+    }
+    return numbers
+  }
 
+  createArray = (size, item) => {
+    let array = []
+    for (let i = 0; i<size; i++) {
+      array.push(item)
+    }
+    return array
+  }
+
+  createBombNumbers = (maxNumber, numberList) => {
+    let bombNumbers = []
+    let bombAmount = Math.floor( Math.sqrt( maxNumber+1 ) )
+    // For loop to put out 10 unique numbers (square identifiers)
+    for ( let i = 1; i <= bombAmount; ++i ) {
+      // pick a random integer between 1 and maxNumber
+      let n = Math.floor( ( Math.random() * ( maxNumber - i ) ) )
+      // put the integer into the bombNumbers array
+      bombNumbers.push( numberList[ n ] )
+      // replace the number we pushed with the last number
+      numberList[ n ] = numberList[ maxNumber - i ]
+      /*
+      if the last number is the number we picked, nothing happens
+      we do this because after every iteration,
+      we decrease the upper limit of n by 1
+      */
+    }
+    return bombNumbers
+  }
+
+  setBombs = () => {
+
+  }
 
 }
 
